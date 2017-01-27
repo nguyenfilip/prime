c9a33f951e5d58d261bbff4f4ffb2f9d15ac32a1
Filip Nguyen <nguyen.filip@gmail.com>
2017-01-27 17:31:15 +0100

    mvn with JavaConfig

---
+++ src/main/java/com/mkyong/SpringConfig.java
@@ -0,0 +1,18 @@
+package com.mkyong;
+
+import com.mkyong.user.bo.UserBo;
+import com.mkyong.user.bo.impl.UserBoImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by maslo on 27.1.17.
+ */
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public UserBo user() {
+        return new UserBoImpl();
+    }
+}
